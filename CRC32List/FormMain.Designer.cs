e2edf3efc12929e89075a62d1c4db0f0ae9f2f1b
Vitaliy <mitradis@gmail.com>
2022-05-23 17:58:11 +0300

    ReUpload

---
+++ CRC32List/FormMain.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CRC32List
+{
+    partial class FormMain
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader filesName;
+        private System.Windows.Forms.ColumnHeader fileID;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.filesName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.fileID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(4, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Файл(ы)";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // listView2
+            // 
+            this.listView2.AllowDrop = true;
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.filesName,
+            this.fileID});
+            this.listView2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView2.FullRowSelect = true;
+            this.listView2.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listView2.Location = new System.Drawing.Point(5, 5);
+            this.listView2.MultiSelect = false;
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(357, 332);
+            this.listView2.TabIndex = 2;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.DragDrop += new System.Windows.Forms.DragEventHandler(this.listView2_DragDrop);
+            this.listView2.DragEnter += new System.Windows.Forms.DragEventHandler(this.listView2_DragEnter);
+            // 
+            // filesName
+            // 
+            this.filesName.DisplayIndex = 1;
+            this.filesName.Text = "Файлы:";
+            this.filesName.Width = 245;
+            // 
+            // fileID
+            // 
+            this.fileID.DisplayIndex = 0;
+            this.fileID.Text = "CRC32:";
+            this.fileID.Width = 85;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(267, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Закрыть";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(144, 342);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 34);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 381);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRC32 List";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
+
---
+++ CRC32List/FormMain.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CRC32List
+{
+    public partial class FormMain : Form
+    {
+        List<string> cacheFile = new List<string>();
+        string lastFolder = null;
+        int mouseWindowX = 0;
+        int mouseWindowY = 0;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (lastFolder != null)
+            {
+                if (Directory.Exists(lastFolder))
+                {
+                    openFileDialog1.InitialDirectory = lastFolder;
+                }
+            }
+            else
+            {
+                openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                lastFolder = Path.GetDirectoryName(openFileDialog1.FileName);
+                listView2.Items.Clear();
+                cacheFile.Clear();
+                cacheFile.AddRange(openFileDialog1.FileNames);
+                funcGet();
+            }
+        }
+
+        private void funcGet()
+        {
+            foreach (string line in cacheFile)
+            {
+                if (File.Exists(line))
+                {
+                    string[] row = { Path.GetFileName(line), getCRC(line) };
+                    var listViewItem = new ListViewItem(row);
+                    listView2.Items.Add(listViewItem);
+                }
+            }
+            filesName.Text = "Файлы (" + listView2.Items.Count.ToString() + "):";
+        }
+
+        private string getCRC(string file)
+        {
+            FileStream streamFile = File.OpenRead(file);
+            string line = string.Format("{0:X}", calculateCRC(streamFile));
+            streamFile.Close();
+            while (line.Length != 8)
+            {
+                line = "0" + line;
+            }
+            return line;
+        }
+
+        private uint calculateCRC(Stream stream)
+        {
+            const int buffer_size = 1024;
+            const uint POLYNOMIAL = 0xEDB88320;
+            uint result = 0xFFFFFFFF;
+            uint Crc32;
+            byte[] buffer = new byte[buffer_size];
+            uint[] table_CRC32 = new uint[256];
+            unchecked
+            {
+                for (int i = 0; i < 256; i++)
+                {
+                    Crc32 = (uint)i;
+                    for (int j = 8; j > 0; j--)
+                    {
+                        if ((Crc32 & 1) == 1)
+                        {
+                            Crc32 = (Crc32 >> 1) ^ POLYNOMIAL;
+                        }
+                        else
+                        {
+                            Crc32 >>= 1;
+                        }
+                    }
+                    table_CRC32[i] = Crc32;
+                }
+                int count = stream.Read(buffer, 0, buffer_size);
+                while (count > 0)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        result = ((result) >> 8) ^ table_CRC32[(buffer[i]) ^ ((result) & 0x000000FF)];
+                    }
+                    count = stream.Read(buffer, 0, buffer_size);
+                }
+            }
+            stream.Close();
+            return ~result;
+        }
+
+        private void listView2_DragDrop(object sender, DragEventArgs e)
+        {
+            listView2.Items.Clear();
+            cacheFile.Clear();
+            cacheFile.AddRange((string[])e.Data.GetData(DataFormats.FileDrop));
+            funcGet();
+        }
+
+        private void listView2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseWindowX = e.X + 147;
+            mouseWindowY = e.Y + 344;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Location = new Point(Cursor.Position.X - mouseWindowX, Cursor.Position.Y - mouseWindowY);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            pictureBox1.MouseMove -= pictureBox1_MouseMove;
+        }
+    }
+}
